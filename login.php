f0f2bf7ad7adf77ec8937b3eef47ff1ed410f1dc
Daniyal Farooque <56557002+DaniyalFarooque@users.noreply.github.com>
2021-06-15 20:44:35 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 //login.php
 
 // connect database
-include('../TETRIS/database/database_connection.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/TETRIS/database/database_connection.php');
 
 session_start();
 
